8ed8db5ba423a2e394fc975f74196111b4eb84e5
DTW2023 <martin_flint@dell.com>
2023-04-05 10:53:51 -0400

    changed messages

--- 7 - Generate Load/run.sh
+++ 7 - Generate Load/run.sh
@@ -4,9 +4,9 @@ echo firing up a load for PowerScale
 cd powerscale
 ansible-playbook -i inventory playbook.yaml
 echo use 
-echo kubectl scale --replicas=X statefulsets.apps/fio -n fio-powerscale 
+echo kubectl scale --replicas=X statefulsets.apps/fio -n NAMESPACE 
 echo to scale up or down I/O workloads
-echo to delete use kubectl delete ns fio-powerscale
+echo to delete use kubectl delete ns NAMESPACE
 echo "------------------------------------------------------------------"
 echo ""
 
@@ -14,9 +14,9 @@ cd ../powerstore
 echo firing up a load for PowerStore
 ansible-playbook -i inventory playbook.yaml
 echo use 
-echo kubectl scale --replicas=X statefulsets.apps/fio -n fio-powerstore 
+echo kubectl scale --replicas=X statefulsets.apps/fio -n NAMESPACE 
 echo to scale up or down I/O workloads
-echo to delete use kubectl delete ns fio-powerstore
+echo to delete use kubectl delete ns NAMESPACE
 echo "------------------------------------------------------------------"
 echo ""
 
