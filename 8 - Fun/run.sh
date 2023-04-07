3309d18124844dcd660f678a4293a8fcd1235375
DTW2023 <martin_flint@dell.com>
2023-04-07 11:55:48 -0400

    changed ip addrs back

--- 8 - Fun/run.sh
+++ 8 - Fun/run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-echo Deploying MineCraft -  connect to minecraft at 10.204.20.174:25565
+echo Deploying MineCraft -  connect to minecraft at 10.204.20.173:25565
 cd mc
 ansible-playbook -i inventory playbook.yaml
 cd ../qk
-echo Deploying Quake - connect at http://10.204.20.175
+echo Deploying Quake - connect at http://10.204.20.174
 ansible-playbook -i inventory playbook.yaml
 cd ..
