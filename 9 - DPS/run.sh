8eed8a9a49da41ea9adc94e2e04537d9faedf44b
DTW2023 <martin_flint@dell.com>
2023-04-13 15:28:34 -0400

    added a comment

--- 9 - DPS/run.sh
+++ 9 - DPS/run.sh
@@ -1,1 +1,6 @@
+# martin.flint@dell.com
+# for some reason I can't set the ansible_ssh_pass in inventory, or vars.yaml and use something like
+# ansible_ssh_pass: "{{ lookup('env', 'DD_PASSWORD') }}"
+# do I pass the setting on the ansible-playbook cli, that seems to work. I can also hard doee it, but don't really wanna do that!
+
 ansible-playbook -i inventory playbook.yaml -e "ansible_ssh_pass=$DD_PASSWORD"
