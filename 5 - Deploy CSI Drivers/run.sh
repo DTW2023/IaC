78dbce223858c8208261270d4d4daad00979c50e
DTW2023 <martin_flint@dell.com>
2023-04-17 12:13:47 -0400

    update

--- 5 - Deploy CSI Drivers/run.sh
+++ 5 - Deploy CSI Drivers/run.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 echo ""
+
+cd deploy_powerstore
+echo Deploying PowerStore CSI Driver
+ansible-playbook -i inv playbook.yaml
+
+cd ..
+
 echo Deploying PowerScale CSI Driver
 cd deploy_powerscale
 ansible-playbook -i inv playbook.yaml
 echo ""
-cd ../deploy_powerstore
-echo Deploying PowerStore CSI Driver
-ansible-playbook -i inv playbook.yaml
+
 
 echo Drivers installed and configured
